6cd5bc94a95535623357b9e3ff84362d023b492d
JackOfBlades232 <petrkiyashko232@gmail.com>
2025-09-22 12:31:08 +0300

    Init scanner stuff

--- cpplox/include/cpplox/defs.hpp
+++ cpplox/include/cpplox/defs.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
+#pragma clang diagnostic ignored "-Wunqualified-std-cast-call"
+
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <sys/types.h>
+
+#include <utility>
+#include <iostream>
+#include <print>
+#include <span>
+
+#include <version.hpp>
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -19,3 +29,22 @@ using uint = unsigned int;
 using uchar = unsigned char;
 using usize = size_t;
 using isize = ssize_t;
+
+// Don't need all the colonoscopy, and my type naming conventions are already
+// different from the std ones.
+using namespace std;
+
+namespace detail
+{
+
+template <class TF>
+struct Defer {
+    TF f;
+
+    Defer(TF &&f) : f{move(f)} {}
+    ~Defer() { f(); }
+};
+
+}
+
+#define DEFER(stmt_) detail::Defer defer__ ## __COUNTER__ {[&] { stmt_; }}
--- cpplox/src/main.cpp
+++ cpplox/src/main.cpp
@@ -1,11 +1,123 @@
 #include <defs.hpp>
-#include <version.hpp>
-#include <print>
 
-int main()
+enum errc_t {
+    e_ec_ok = 0,
+    e_ec_arg_error = 1,
+    e_ec_sys_error = 2,
+    e_ec_code_error = 13,
+};
+
+struct lox_t {
+    bool had_error = false;
+};
+
+void report(lox_t &lox, int line, string_view location, string_view message)
 {
-    u32 const major_ver = MAJOR_VERSION, minor_ver = MINOR_VERSION;
-    std::println(
-        "Hello, world! This is jb-cpplox, version {} (major={}, minor={}).",
-        VERSION, major_ver, minor_ver);
+    println(stderr, "[line {}] Error {}: {}", line, location, message);
+    lox.had_error = true;
+}
+
+void error(lox_t &lox, int line, string_view message)
+{
+    report(lox, line, "", message);
+}
+
+// @TODO
+struct token_t : string_view {};
+
+vector<token_t> scan_tokens(string_view code, lox_t &lox)
+{
+    // @TODO
+    return {};
+}
+
+errc_t run(string_view code, lox_t &lox)
+{
+    vector<token_t> tokens = scan_tokens(code, lox);
+    if (lox.had_error)
+        return e_ec_code_error;
+    
+    // @TEST
+    for (auto const &tok : tokens)
+        println("{}", tok);
+
+    return e_ec_ok;
+}
+
+optional<string> read_whole_file(char const *fn)
+{
+    FILE *f = fopen(fn, "rb");
+    if (!f) {
+        perror(fn);
+        return nullopt;
+    }
+    DEFER(fclose(f));
+
+    optional<string> res;
+    res.emplace();
+
+    fseek(f, 0, SEEK_END);
+    usize len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    res->resize(len);
+    if (fread(res->data(), len, 1, f) != 1) {
+        perror(fn);
+        return nullopt;
+    }
+        
+    return res;
+}
+
+errc_t run_file(char const *fn, lox_t &lox)
+{
+    if (auto bytes = read_whole_file(fn))
+        return run(*bytes, lox);
+    else
+        return e_ec_sys_error;
+}
+
+errc_t run_prompt(lox_t &lox)
+{
+    for (;;) {
+        print("> ");
+        string line{};
+
+        getline(cin, line);
+        if (!cin.good())
+            break;
+
+        run(line, lox);
+        lox.had_error = false;
+    }
+
+    if (!cin.eof()) {
+        println(stderr, "Input error (unknown)");
+        return e_ec_sys_error;
+    }
+
+    return e_ec_ok;
+}
+
+void show_header()
+{
+    println("jb-cpplox, version {}", VERSION);
+}
+
+int main(int argc, char **argv)
+{
+    span<char const *const> args{argv + 1, usize(argc - 1)};
+
+    lox_t lox{};
+    
+    if (args.size() > 1) {
+        show_header();
+        println(stderr, "Usage: cpplox [file]");
+        return e_ec_arg_error;
+    } else if (args.size() == 1) {
+        return run_file(args[0], lox);
+    } else {
+        show_header();
+        return run_prompt(lox);
+    }
 }
